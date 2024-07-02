136419be0a36a66d61e532fa5e226deb31b3c923
bp0002 <bp950722@outlook.com>
2024-07-02 20:55:03 +0800

    # fix: quad_tree

--- crates/wasm_engine/src/lib.rs
+++ crates/wasm_engine/src/lib.rs
@@ -3,7 +3,7 @@ use std::panic;
 pub use pi_export_gui::*;
 // pub use pi_export_astar::export:: *;
 pub use pi_export_base::export::*;
-// pub use pi_export_quad_tree::export::*;
+pub use pi_export_quad_tree::export::*;
 pub use pi_spine_export:: *;
 pub use scene3d_export::{
 	engine::*,
