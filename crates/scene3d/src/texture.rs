1057b9e8dea0ed5e70f682adbab13c04d06398dd
bp0002 <bp950722@outlook.com>
2024-11-18 11:03:45 +0800

    # fix: Data Texture Update Sub; Texture Combine

--- crates/scene3d/src/texture.rs
+++ crates/scene3d/src/texture.rs
@@ -1,21 +1,23 @@
 use std::ops::Deref;
 use pi_assets::asset::{Handle, Size};
-use pi_bevy_asset::ShareAssetMgr;
-use pi_bevy_render_plugin::{PiRenderDevice, PiRenderQueue};
-use pi_export_base::export::{DataTextureRecord, DataTextureRefs};
-use pi_hash::{XHashMap, XHashSet};
-use pi_scene_context::pass::{Resource, WorldResourceTemp};
-use pi_scene_shell::prelude::{ResImageTexture, KeyImageTexture, Res, ResMut};
+use pi_export_base::export::DataTextureSubData;
+use pi_hash::XHashMap;
+use pi_scene_context::pass::{TextureCombineCmds, WorldResourceTemp};
+use pi_scene_shell::prelude::{ResImageTexture, KeyImageTexture};
 pub use pi_export_base::{export::{Engine, Atom}, constants::*};
 // use pi_render::asset::TAssetKeyU64;
 
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::wasm_bindgen;
 use js_proxy_gen_macro::pi_js_export;
-use wgpu::TextureFormat;
 
-use crate::constants::EngineConstants;
+use crate::{constants::EngineConstants, mesh::CommandsExchangeD3};
 pub use crate::engine::ActionSetScene3D;
+
+pub struct CombineTextureAtlas {
+    keys: Vec<Atom>,
+    frames: Vec<(u32, u32, u32, u32)>,
+}
 
 #[cfg_attr(target_arch="wasm32", wasm_bindgen)]
 #[pi_js_export]
@@ -23,41 +25,57 @@ pub struct DataTextureRes(Handle<ResImageTexture>);
 
 #[cfg_attr(target_arch="wasm32", wasm_bindgen)]
 #[pi_js_export]
-pub fn p3d_create_data_texture(app: &mut Engine, param: &mut ActionSetScene3D, key: &Atom, data: &[u8], width: f64, height: f64, format: f64, size_per_pixel: f64) {
-
-	pi_export_base::export::await_last_frame(app);
+pub fn p3d_create_data_texture(param: &mut CommandsExchangeD3, key: &Atom, width: f64, height: f64, format: f64, aspect: Option<f64>) {
 
     let key = key.deref().clone();
     let format = EngineConstants::render_color_format(format).val();
-    let size_per_pixel = size_per_pixel as u32;
     let width = width as u32;
     let height = height as u32;
     let dimension = wgpu::TextureViewDimension::D2;
     let is_opacity = true;
-    let texkey = KeyImageTexture { url: key.clone(), srgb: false, file: false, compressed: false, depth_or_array_layers: 0, useage: wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::TEXTURE_BINDING };
+    let useage = wgpu::TextureUsages::COPY_DST | wgpu::TextureUsages::TEXTURE_BINDING;
+    let texkey = KeyImageTexture { url: key.clone(), srgb: false, file: false, compressed: false, depth_or_array_layers: 0, useage };
 
-    let refs = app.world.get_resource_mut::<DataTextureRefs>().unwrap();
-    refs.data.insert(key.clone(), data.to_vec());
-    let record = app.world.get_resource_mut::<DataTextureRecord>().unwrap();
-    record.creation.insert(key.clone(), (width, height, size_per_pixel, format, dimension, texkey));
+    let info = DataTextureSubData {
+        data: None,
+        dataoffset: 0,
+        xoffset: 0,
+        yoffset: 0,
+        width, height,
+        aspect: None,
+        depth_or_array_layers: 0
+    };
+    param.datatexcmd.createdata.insert(key.clone(), (info, format, dimension, texkey));
 }
 
 #[cfg_attr(target_arch="wasm32", wasm_bindgen)]
 #[pi_js_export]
-pub fn p3d_update_data_texture(app: &mut Engine, param: &mut ActionSetScene3D, key: &Atom, data:&[u8]) {
-	pi_export_base::export::await_last_frame(app);
+pub fn p3d_update_data_texture(param: &mut CommandsExchangeD3, key: &Atom, data:&[u8], xoffset: f64, yoffset: f64, width: f64, height: f64, aspect: Option<f64>) {
 
-    let refs = app.world.get_resource_mut::<DataTextureRefs>().unwrap();
-    refs.data.insert(key.deref().clone(), data.to_vec());
+    let key = key.deref().clone();
+    if param.datatexcmd.updatedata.contains_key(&key) == false {
+        param.datatexcmd.updatedata.insert(key.clone(), vec![]);
+    }
+    if let Some(list) = param.datatexcmd.updatedata.get_mut(&key) {
+        list.push(DataTextureSubData {
+            data: Some(data.to_vec()),
+            dataoffset: 0,
+            xoffset: xoffset as u32,
+            yoffset: yoffset as u32,
+            width: width as u32, height: height as u32,
+            aspect: None,
+            depth_or_array_layers: 0
+        });
+    }
 }
 
 #[cfg_attr(target_arch="wasm32", wasm_bindgen)]
 #[pi_js_export]
-pub fn p3d_remove_data_texture(app: &mut Engine, key: &Atom) {
-	pi_export_base::export::await_last_frame(app);
-
-    let record = app.world.get_resource_mut::<DataTextureRecord>().unwrap();
-    record.record.remove(key.deref());
+pub fn p3d_remove_data_texture(param: &mut CommandsExchangeD3, key: &Atom) {
+    let key = key.deref().clone();
+    param.datatexcmd.createdata.remove(&key);
+    param.datatexcmd.updatedata.remove(&key);
+    param.datatexcmd.record.remove(&key);
 }
 
 #[cfg_attr(target_arch="wasm32", wasm_bindgen)]
@@ -77,4 +95,42 @@ pub fn p3d_query_texture(app: &mut Engine, param: &mut ActionSetScene3D, isfile:
     } else {
         false
     }
+}
+
+#[cfg_attr(target_arch="wasm32", wasm_bindgen)]
+#[pi_js_export]
+pub fn p3d_texture_combine_add_frame(cmd: &mut CommandsExchangeD3, requestid: f64, targetkey: &Atom, file: &Atom, idx: f64, iscompress: bool, xoffset: f64, yoffset: f64, width: f64, height: f64) {
+    let requestid = requestid as u32;
+    let targetkey = targetkey.deref().clone();
+    let file = file.deref().clone();
+    if cmd.combinecmds.contains_key(&requestid) == false {
+        cmd.combinecmds.insert(requestid, (targetkey.clone(), XHashMap::default()));
+    }
+    if let Some(cmd) = cmd.combinecmds.get_mut(&requestid) {
+        cmd.1.insert(file, (requestid, idx as u16, iscompress, xoffset as u32, yoffset as u32, width as u32, height as u32));
+    }
+}
+#[cfg_attr(target_arch="wasm32", wasm_bindgen)]
+#[pi_js_export]
+pub fn p3d_texture_combine_remove(app: &mut Engine, cmd: &mut CommandsExchangeD3, requestid: f64, targetkey: &Atom) {
+    pi_export_base::export::await_last_frame(app);
+    cmd.combinecmds.remove(&(requestid as u32));
+    app.world.get_resource_mut::<TextureCombineCmds>().unwrap().remove(requestid as u32, targetkey.deref().clone());
+}
+#[cfg_attr(target_arch="wasm32", wasm_bindgen)]
+#[pi_js_export]
+pub fn p3d_texture_combine_query(app: &mut Engine, success: &mut [u32], fail: &mut [u32]) {
+    pi_export_base::export::await_last_frame(app);
+    
+    let cmds = app.world.get_resource_mut::<TextureCombineCmds>().unwrap();
+    let mut idx = 0;
+    cmds.successed().for_each(|id| {
+        success[idx] = id;
+        idx += 1;
+    });
+    let mut idx = 0;
+    cmds.failed().for_each(|id| {
+        fail[idx] = id;
+        idx += 1;
+    });
 }
