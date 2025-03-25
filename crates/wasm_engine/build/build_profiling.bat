fc16dfe3cfd98a80721fb0a3b97a15546f28a065
wzjsun <530739162@qq.com>
2025-03-25 15:28:17 +0800

    fix: 编译问题

--- crates/wasm_engine/build/build_profiling.bat
+++ crates/wasm_engine/build/build_profiling.bat
@@ -5,7 +5,7 @@ set RUSTFLAGS=--cfg=web_sys_unstable_apis
 set RUSTFLAGS=--cfg getrandom_backend="wasm_js"
 set RUST_LOG=info
 wasm-pack build --profiling  --target web --out-dir pkg_profiling --out-name wasm_engine --features release
-C:\\Users\\chuanyan\\.cargo\\bin\\wasm-bindgen.exe ../../target/wasm32-unknown-unknown/release/pi_wasm_engine.wasm --out-dir pkg_profiling --typescript --target web --out-name wasm_engine
+C:\\Users\\chuanyan\\AppData\\Local\\.wasm-pack\\wasm-bindgen-53edf4f5acf7b49d\\wasm-bindgen.exe ../../target/wasm32-unknown-unknown/release/pi_wasm_engine.wasm --out-dir pkg_profiling --typescript --target web --out-name wasm_engine
 node build/build_wasm.js pkg_profiling wasm_engine
 pause;
 
