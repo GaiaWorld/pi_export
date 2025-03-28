2b077661efad85f9eb5c0a91acd6a55eb29065e2
bp0002 <bp950722@outlook.com>
2025-03-28 14:37:13 +0800

    # fix: scene pick param

--- crates/scene3d/src/engine.rs
+++ crates/scene3d/src/engine.rs
@@ -218,7 +218,7 @@ pub struct ActionSetScene3D {
     pub(crate) nodesdirectlight: QueryState<(&'static DirectLight), ()>,
     pub(crate) nodespointlight: QueryState<(&'static PointLight), ()>,
     pub(crate) collider: QueryState<(&'static SceneColliderPool, &'static SceneBoundingPool), ()>,
-    pub(crate) pickitems: QueryState<(&'static RenderQueueSortParam, &'static GlobalEnable), ()>,
+    pub(crate) pickitems: QueryState<&'static GlobalEnable, ()>,
     
     // pub(crate) uniforms: QueryState<&'static BindEffect>,
     // pub(crate) animatorablefloat: QueryState<&'static AnimatorableFloat>,
