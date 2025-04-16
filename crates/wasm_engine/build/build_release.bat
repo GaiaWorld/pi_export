d7ac89daf2fc792ac1b00318255ce50a782f34f0
zhuy <zhuy>
2025-04-16 17:53:54 +0800

    feat: 编译参数

--- crates/wasm_engine/build/build_release.bat
+++ crates/wasm_engine/build/build_release.bat
@@ -3,7 +3,7 @@ call cfg.bat
 cd ../
 @REM set RUSTFLAGS=--cfg=web_sys_unstable_apis
 @REM set RUSTFLAGS=-Zlocation-detail=none
-@REM 小游戏不支持-reference-types,-sign-ext优化，  参数参考https://blog.rust-lang.org/2024/09/24/webassembly-targets-change-in-default-target-features.html
+@REM 微信小游戏不支持-reference-types,-sign-ext优化，  参数参考https://blog.rust-lang.org/2024/09/24/webassembly-targets-change-in-default-target-features.html
 set RUSTFLAGS=--cfg getrandom_backend="wasm_js" -Ctarget-feature=-reference-types,-sign-ext
 set CARGO_UNSTABLE_BUILD_STD=panic_abort,std
 set RUST_LOG=info
