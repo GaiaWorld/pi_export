eb5fe07d061fdb3b80cb495c4da9b0469019ca0c
wzjsun <530739162@qq.com>
2025-06-16 17:58:30 +0800

    fix：

--- crates/timer/src/exports.rs
+++ crates/timer/src/exports.rs
@@ -8,7 +8,7 @@ use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
 // #[pi_js_export]
-pub struct Timer(Timer1<f64, 250, 60, 2>, u64);
+pub struct Timer(Timer1<f64, 128, 60, 2>, u64);
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
 // #[pi_js_export]
@@ -16,7 +16,7 @@ impl Timer {
 	/// 创建定时器
 	// #[pi_js_export]
 	pub fn new() -> Self {
-		Self(Timer1::<f64, 250, 60, 2>::default(), 0)
+		Self(Timer1::<f64, 128, 60, 2>::default(), 0)
 	}
 
 	/// push一个定时任务
@@ -36,15 +36,7 @@ impl Timer {
 	/// 弹出一个定时任务
 	// #[pi_js_export]
 	pub fn pop(&mut self, now: u64) -> Option<f64> {
-		let now: u64 = now / 4; // 以4ms为精度
-		self.0.pop(now + self.1)
-	}
-	
-
-	/// 准备弹出， 在循环弹出前， 先调用此方法
-	// #[pi_js_export]
-	pub fn pop_ready(&mut self) {
-		self.1 = self.0.roll_count();
+		self.0.pop(now)
 	}
 }
 
